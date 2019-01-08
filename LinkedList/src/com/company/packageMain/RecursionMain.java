b1a7dc090eb623955f5a620fab7b962f850d72ca
Shantanu Ghosh <beingshantanu2406@gmail.com>
2019-01-09 00:56:20 +0530

    commit

--- LinkedList/src/com/company/packageMain/RecursionMain.java
+++ LinkedList/src/com/company/packageMain/RecursionMain.java
@@ -3,12 +3,22 @@ package com.company.packageMain;
 public class RecursionMain {
     static int l = 0;
     static int multiplier = 10;
+    static int digit = 0;
+    static int aLength = 0;
 
     static int fact(int n) {
         if (n == 1)
             return 1;
         else
             return n * fact(n - 1);
+    }
+
+    static int sum(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n + sum(n - 1);
+        }
     }
 
     static int numRev(int n) {
@@ -23,7 +33,19 @@ public class RecursionMain {
         }
     }
 
+    static int digitCal(int n) {
+        if (n > 10) {
+            digitCal(n / 10);
+        }
+        return ++digit;
+
+    }
+
+
+
+
     public static void main(String[] args) {
-        System.out.println(numRev(104011));
+        int[] a = {1,2,5,3};
+        //System.out.println(max(a));
     }
 }
